ae06b52776112dfe9dbcf30c0004d65ef1edb832
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-04-01 00:04:38 +0300

    1

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,47 +4,23 @@ import "izitoast/dist/css/iziToast.min.css";
 const submitButton = document.querySelector('button');
 const fulfilled = document.querySelector('input[value="fulfilled"]');
 submitButton?.addEventListener('click', handleSubmit);
-const delayInput = document.querySelector('input[name="delay"')
+const delayInput = document.querySelector('input[name="delay]')
 
 
 
 function handleSubmit(event) {
-  event.preventDefault();
-  const { delay, state } = event.target.elements;
-  const delayTime = delay.value;
-  const choice = state.value;
+    event.preventDefault();
+    const delay = delayInput.value
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+         if (fulfilled.checked) {
+            resolve();
+        } else { reject(); }
 
-  createPromise(delayTime, choice)
-    .then(value => {
-      console.log(value);
-    })
-    .catch(error => {
-      console.log(error);
+    }, delay)
+       
+        
     });
-  form.reset();
-}
-
-function createPromise(delayTime, choice) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (choice === 'fulfilled') {
-        resolve(
-          iziToast.show({
-            ...iziSuccessOptions,
-            message: `Fulfilled promise in ${delayTime}ms`,
-          })
-        );
-      } else {
-        reject(
-          iziToast.show({
-            ...iziRejectOptions,
-            message: `Rejected promise in ${delayTime}ms`,
-          })
-        );
-      }
-    }, delayTime);
-  });
-}
 
     promise.then(() => {
         showNotification(`✅ Fulfilled promise in ${delay}ms`, '#59a10d');
@@ -52,6 +28,8 @@ function createPromise(delayTime, choice) {
         .catch(() => {
             showNotification(`❌ Rejected promise in ${delay}ms`, '#ef4040');
         });
+
+}
 
 function showNotification(message, backgroundColor) {
     iziToast.show({

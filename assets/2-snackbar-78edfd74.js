6fcecd6feff8e9bb23b9435359c112b5955bad49
Santyaga7 <149270626+Santyaga7@users.noreply.github.com>
2024-03-31 19:42:31 +0000

    Deploying to gh-pages from @ Santyaga7/goit-js-hw-10@420ed2dddac0f13a276700dd1f09881f076a6198 🚀

---
+++ assets/2-snackbar-78edfd74.js
@@ -0,0 +1,2 @@
+import{i as n}from"./vendor-77e16229.js";const e=document.querySelector("button");document.querySelector('input[value="fulfilled"]');e==null||e.addEventListener("click",handleSubmit);document.querySelector('input[name="delay"');promise.then(()=>{o(`✅ Fulfilled promise in ${delay}ms`,"#59a10d")}).catch(()=>{o(`❌ Rejected promise in ${delay}ms`,"#ef4040")});function o(i,t){n.show({message:i,messageColor:" #fff",backgroundColor:t,position:"topCenter",messageSize:"16px",messageLineHeight:"150%",iconColor:"white"})}
+//# sourceMappingURL=2-snackbar-78edfd74.js.map
